5f5feecfa3b8e1bd53439a683efa812d242ca9b6
SantiagoV0106 <velascosantiago3@gmail.com>
2023-02-27 17:26:06 -0500

    Conservidor Nrok

--- public - mupi/sketch.js
+++ public - mupi/sketch.js
@@ -1,35 +1,69 @@
-const anchoescenario = 1280
-const largoescenario = 720
-let canvas
-let toro
-let limite
-let tubos = []
-let vellimite = 5
-let gravedad = false
-let fps = 0
+const NGROK = `${window.location.hostname}`;
+let socket = io(NGROK, { path: '/real-time' });
+console.log('172.30.213.141', NGROK);
+
+const anchoescenario = 1280;
+const largoescenario = 720;
+let pantallas = 1;
+let canvas;
+let toro;
+let boton;
+let limite;
+let tubos = [];
+let vellimite = 5;
+let gravedad = false;
+let fps = 0;
 
 function setup() {
   canvas = createCanvas(anchoescenario, largoescenario);
-  windowResized()
+  windowResized();
   toro = new Toro();
-  limite = new Limite()
-  tubos.push(new Tubo)
+  limite = new Limite();
+  tubos.push(new Tubo);
+  boton = createButton('Cambiar Pantalla');
+  boton.position(anchoescenario / 4 * 2.8, largoescenario);
+  boton.mousePressed(cambiaPantallas);
 }
 
 function draw() {
   background(220);
-  toro.pintar()
-  limite.pintar()
-  for (const tubo of tubos) {
-    tubo.pintar()
-  }
+  
+  switch (pantallas) {
+    case 1:
+      textSize(30);
+      text('Aqui va el código QR del mupi', anchoescenario / 3, 200);     
+      rect(480, 260, 300,300);
+      break;
+      
+      case 2:      
+      text('Aquí van las instrucciones', 460, 360);
+      break;
+      
+      case 3 :
+        boton.style('display', 'none');
+        toro.pintar();
+        limite.pintar();
+        for (const tubo of tubos) {
+          tubo.pintar()
+        }
+      
+        if (fps * vellimite % 400 == 0) {
+          tubos.push(new Tubo)
+        }
+      
+        if (gravedad) {
+          fps ++
+        }
 
-  if (fps * vellimite % 400 == 0) {
-    tubos.push(new Tubo)
-  }
+        break;
 
-  if (gravedad) {
-    fps ++
+        case 4: 
+        perder();
+        fill(51);
+        text('Aquí perdiste y procede el formulario', 460, 360);
+        
+        break;
+        
   }
 }
 
@@ -38,15 +72,34 @@ function windowResized(){
     canvas.style('transform', `scale(${windowWidth / width})`)
   }
 }
+
 function keyPressed() {
-  click()
+  click();
 }
 
-function mousePressed() {
-  click()
+function mousePressed() { 
+  boton.mousePressed(cambiaPantallas);
+  click();
+}
+
+function cambiaPantallas() {
+  switch (pantallas) {
+    case 1:      
+        pantallas = 2
+       break;
+       case 2: 
+      pantallas = 3
+      break;
+      case 3:
+        pantallas = 4
+       default:
+      break;
+  }
+  
 }
 
 
+//aqui se agrega la función que conecta con el cel
 function click(){
   if (gravedad) {
     toro.gravedadtoro.set(createVector(0,-5))
@@ -60,7 +113,9 @@ function click(){
 
 function perder() {
   gravedad = false
+  pantallas = 4
 }
+
 
 function Toro() {
     this. radio = 60
@@ -132,5 +187,7 @@ function Tubo() {
     ]
   }
 }
-
+socket.on('display-Hola', messages => {
+  console.log(messages)
+})
 
